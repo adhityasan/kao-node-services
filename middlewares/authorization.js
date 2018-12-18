23027f2d0c7c21db40a324b05c8b76823234df2f
Adhitya Yosua Sanjaya <adhityad3v@gmail.com>
2018-12-18 12:26:07 +0700

    add token prefix on authorization check

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -3,9 +3,13 @@ const config = require('config')
 
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token')
+  const prefix = config.get('tokenPrefix')
   
   if (!token) return res.status(401).send('Access Denied. No token provided')
 
+  const containPrefix = token.includes(prefix)
+
+  if (!containPrefix) return res.status(400).send('Invalid token. No caller identifier')
   try {
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'))
     req.user = decoded
