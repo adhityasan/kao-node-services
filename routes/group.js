7d716d6b4a2d0b3bdaa061adae1eafcfc54dd423
Adhitya Yosua Sanjaya <adhityad3v@gmail.com>
2018-12-19 16:51:12 +0700

    create group action

--- routes/group.js
+++ routes/group.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { Group } = require('@models/groups')
+const { Group, joiSchema_Group } = require('@models/groups')
 
 const authorize = require('@middlewares/authorization')
 const mongoosedocquery = require('@utils/mongoose-doc-query')
+const { joiValidate, buildErrorResponse } = require('@utils/joi-validate')
 
 async function getGroups(req, res) {
   let docquery = mongoosedocquery(req.query)
@@ -22,6 +23,23 @@ async function getGroups(req, res) {
   }
 }
 
+async function createGroup(req, res) {
+  const data = req.body
+  const { error: joiError } = joiValidate(data, joiSchema_Group)
+
+  if (joiError) return res.status(400).send(buildErrorResponse(joiError))
+
+  try {
+    const newgroup = new Group(data)
+    newgroup.save()
+
+    return res.send({ message: 'Success create new group', data: newgroup })
+  } catch (error) {
+    return res.status(400).send({ message: 'Fail create new group', error: error })    
+  }
+}
+
 router.get('/', authorize, getGroups)
+router.post('/', authorize, createGroup)
 
 module.exports = router
